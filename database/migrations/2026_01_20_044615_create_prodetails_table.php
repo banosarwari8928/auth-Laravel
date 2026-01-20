903ffe0a6f4de9947f3e16e601106e51431e6f7c
banosarwari8928 <banosarwari8928@gmail.com>
2026-01-20 09:20:21 +0430

    product details  migration,model,view added

---
+++ database/migrations/2026_01_20_044615_create_prodetails_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prodetails', function (Blueprint $table) {
+            $table->id();
+            $table->integer("price");
+            $table->string("describtion");
+            $table->integer("quantity");
+            $table->string("made_in");
+            $table->forgeinId('productId')->constrained("products");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prodetails');
+    }
+};
